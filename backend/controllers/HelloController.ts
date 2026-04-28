f54401be62e01b72e00d6b7db6bea675f58fbfd5
Whitespace <94882267+whitespace-v@users.noreply.github.com>
2026-04-28 13:40:57 +1000

    Update HelloController.ts

--- backend/controllers/HelloController.ts
+++ backend/controllers/HelloController.ts
@@ -1,11 +1,11 @@
 import { Request, Response } from "express";
-import { EmptyQuery, HelloReponse } from "../types/hello";
+import { EmptyQuery, EmptyParams, HelloResponse } from "../types/hello";
 
 export class HelloController{
     public static getHello = 
     (
-        _req: Request<never, HelloReponse, EmptyQuery>, 
-        res: Response<HelloReponse>
+        _req: Request<never, EmptyParams, EmptyQuery>, 
+        res: Response<HelloResponse>
     ): void => {
         res.status(200).json({message: "Hello !"})
     }
